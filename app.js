ce3f43f0e4c537ae06c9623ddbf506ff57121682
Yuest Wang <yuestwang@gmail.com>
2011-12-22 16:49:07 +0800

    ready for proxyTamper()

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express')
+  , connect = require('connect')
   , routes = require('./routes')
   , proxy = require('./lib/proxy')
   , io = require('socket.io')
+  , u = require('./lib/u')
 
 var app = module.exports = express.createServer();
 
@@ -9,13 +11,39 @@ var mc
 try {
   mc = require('./mcconfig.js')
 } catch (e) {
-  mc = { prefix: '/__mc'
-       , lib: __dirname + '/public/matcha'
-       , ajax: __dirname + '/mcajax'
-       , test: __dirname + '/mctest' }
+  mc =  { prefix: '/__mc'
+        , lib: __dirname + '/public/matcha'
+        , ajax: __dirname + '/mcajax'
+        , test: __dirname + '/mctest'
+        , port: 8088 }
 }
 
-// Configuration
+var proxyHost = function(host) {
+  return function (_, req) {
+    return !req.url.indexOf(mc.prefix) ? false : host
+  }
+}
+
+var proxyTamper = function(body) {
+  return body;
+}
+
+if (!(mc.proxy instanceof connect.HTTPServer)) {
+  mc._proxy = Array.isArray(mc.proxy) ? mc.proxy : []
+  mc.proxy = connect()
+  mc._proxy.forEach(function(option) {
+    mc.proxy.use(proxy.http(u.extend(option, { host: proxyHost(option.host), tamper: proxyTamper })))
+  })
+  mc.proxy.use(proxy.http(
+    { host: function(host, req) {
+        return !req.url.indexOf(mc.prefix + '/blank') ? host : false
+      }
+    , path: function(path) {
+        return path.replace(mc.prefix + '/blank', mc.prefix + '/_blank')
+      }
+    }))
+  mc.proxy.use(proxy.http())
+}
 
 app.configure(function(){
   app
@@ -31,7 +59,10 @@ app.configure(function(){
     .use(mc.prefix, app.router)
     .use(mc.prefix, express['static'](__dirname + '/public'))
     .use(mc.prefix + '/lib', express['static'](mc.lib))
-    .use(mc.proxy || proxy.http())
+    .use(mc.proxy)
+    .use(function(req, res, next) {
+           res.send('/* Cannot ' + req.method + ' ' + req.url + ' */', 404);
+         })
 
   app.helpers({
       _p: mc.prefix
@@ -48,10 +79,11 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.index);
+app.get('/_blank', function(req, res){
+  res.render('blank', { layout: false, locals: { title: 'Matcha Blank' }})
+})
 
 io = io.listen(app)
-app.listen(8088);
 
 io.sockets.on('connection', function (socket) {
   console.log('connection');
@@ -68,4 +100,7 @@ io.sockets.on('connection', function (socket) {
     })
 })
 
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+if (require.main === module) {
+  app.listen(mc.port);
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+}
--- mcconfig.example.xueqiu.js
+++ mcconfig.example.xueqiu.js
@@ -2,24 +2,13 @@ var connect = require('connect')
   , proxy = require('./lib/proxy')
 
 var mc = exports = module.exports = {
-    prefix: '/_mc'
-  , proxy: connect()
+    prefix: '/__mc'
+  , proxy: [ { host: '127.0.0.1', port: 7878 }
+           , { host: 'xueqiu.com', port: 80 }
+           ]
   , lib: __dirname + '/public/matcha'
   , ajax: __dirname + '/mcajax'
   , test: __dirname + '/mctest'
+  , port: 8088
 }
 
-function proxyHost(host) {
-  return function (_, req) {
-    return !req.url.indexOf(mc.prefix) ? false : host
-  }
-}
-
-mc.proxy = connect()
-  .use(proxy.http({ host: proxyHost('127.0.0.1'), port: 7878 }))
-  .use(proxy.http({ host: proxyHost('xueqiu.com'), port: 80 }))
-  .use(proxy.http())
-
-if (require.main === module) {
-  mc.proxy.listen(8088);
-}
