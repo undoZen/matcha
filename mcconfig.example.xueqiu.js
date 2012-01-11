61d75efe171b87e7e440fe8bbe29fd531bec2868
Yuest Wang <yuestwang@gmail.com>
2012-01-11 14:21:10 +0800

    proxy static files

--- mcconfig.example.xueqiu.js
+++ mcconfig.example.xueqiu.js
@@ -10,12 +10,16 @@ var mc = exports = module.exports = {
   , port: 8088
 }
 
+connect(connect['static']('/data/deploy/snowball/html')).listen(7890)
+
 var env = process.env.NODE_ENV || 'development'
 
 if ('test' === env) {
   mc.proxy =  [ { host: '192.168.1.198', port: 80 } ]
 } else if ('local' === env) {
-  mc.proxy =  [ { host: '127.0.0.1', port: 7878 } ]
+  mc.proxy =  [ { host: '127.0.0.1', port: 7890 }
+              , { host: '127.0.0.1', port: 7878 }
+              ]
 } else if ('online' === env) {
   mc.proxy =  [ {} ]
 } else if ('test-online' === env) {
@@ -23,17 +27,20 @@ if ('test' === env) {
               , {}
               ]
 } else if ('local-test' === env) {
-  mc.proxy =  [ { host: '127.0.0.1', port: 7878 }
+  mc.proxy =  [ { host: '127.0.0.1', port: 7890 }
+              , { host: '127.0.0.1', port: 7878 }
               , { host: '192.168.1.198', port: 80 }
               ]
 } else if ('local-test-online' === env) {
-  mc.proxy =  [ { host: '127.0.0.1', port: 7878 }
+  mc.proxy =  [ { host: '127.0.0.1', port: 7890 }
+              , { host: '127.0.0.1', port: 7878 }
               , { host: '192.168.1.198', port: 80 }
               , {}
               ]
 } else {
   //defalt: local-online
-  mc.proxy =  [ { host: '127.0.0.1', port: 7878 }
+  mc.proxy =  [ { host: '127.0.0.1', port: 7890 }
+              , { host: '127.0.0.1', port: 7878 }
               , {}
               ]
 }
